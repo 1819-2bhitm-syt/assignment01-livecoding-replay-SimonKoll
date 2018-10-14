9bcd0cec316bf9cf622dd709bc89feda4a5b3e8b
SimonKoll <s.koll@students.htl-leonding.ac.at>
2018-10-14 10:56:11 +0200

    objekte, wert und ref Typ 11.10

---
+++ ReferencType/app.js
@@ -0,0 +1,6 @@
+let x ={value:10};
+let y = x;
+
+x.value = 20;
+console.log("x = ",x);
+console.log("y = ", y);
---
+++ call-pass_by_value/app.js
@@ -0,0 +1,6 @@
+let hallo = function (str){
+    str = "Hallo + " + str;
+};
+let welt ="Welt";
+hallo(welt);
+console.log(welt);
